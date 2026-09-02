18411f1a4f36983845284641e4234c84f2b93c8f
Samin <samin.mhr2004@gmail.com>
2026-09-02 21:37:16 +0330

    fixed a bug.

--- components/headers/Logo.jsx
+++ components/headers/Logo.jsx
@@ -6,18 +6,8 @@ const Logo = () => {
     <Link
       href="/"
       className="
-        logo
-        relative
-        z-30
-        block
-        rounded-lg
-        bg-white
-        text-center
-        shadow-[0px_1px_10px_rgba(0,0,0,0.25)]
-        transition-all
-        duration-500
-        hover:shadow-[0px_1px_10px_rgba(0,0,0,0.5)]
-      "
+        logo relative z-30 block rounded-lg bg-white text-center shadow-[0px_1px_10px_rgba(0,0,0,0.25)]
+        transition-all duration-500 hover:shadow-[0px_1px_10px_rgba(0,0,0,0.5)]"
       style={{
         padding: "1rem",
       }}
