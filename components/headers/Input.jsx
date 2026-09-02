dc9328168084d9c70750d5031f64c09c5806c692
Samin <samin.mhr2004@gmail.com>
2026-09-02 21:40:09 +0330

    added focus style to input.

--- components/headers/Input.jsx
+++ components/headers/Input.jsx
@@ -2,12 +2,22 @@ import { BiSearchAlt } from "react-icons/bi";
 
 const Input = () => {
   return (
-    <div className="flex justify-between items-center  bg-white p-3 rounded-lg shadow-[0px_0px_5px_rgba(0,0,0,0.15)] w-230">
+    <div
+      className="
+        flex justify-between items-center
+        bg-white p-3 rounded-lg
+        shadow-[0px_0px_5px_rgba(0,0,0,0.15)]
+        focus-within:shadow-[0px_0px_12px_rgba(0,0,0,0.3)]
+        transition-shadow duration-300
+        w-230
+      "
+    >
       <input
         name="productSearch"
         id="productSearch"
         type="text"
         placeholder="جست و جو بین محصولات..."
+        className="outline-none"
       />
 
       <label htmlFor="productSearch">
