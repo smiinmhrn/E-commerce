fcb0f6eabb837d91233fa70f8c41b583a26ea0f8
Samin <samin.mhr2004@gmail.com>
2026-09-04 13:34:01 +0330

    fixed header and footer in all pages.

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 
+import Header from "@/components/headers/Header";
+import Footer from "@/components/footer/Footer";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,12 +21,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="fa"
-      dir="rtl"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="fa" dir="rtl">
+      <body>
+        <Header />
+
+        {children}
+
+        <Footer />
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,38 +1,34 @@
 import Banners from "@/components/banners/Banners";
 import BoxSlider from "@/components/boxSlider/BoxSlider";
 import Categories from "@/components/categories/Categories";
-import GraphicalBox from "@/components/graphical/GraphicalBox";
 import GraphicalSlider from "@/components/graphical/GraphicalSlider";
-import Header from "@/components/headers/Header";
 import Swiper from "@/components/swiper/Swiper";
 import WebNews from "@/components/weblognews/WebNews";
-import Footer from "@/components/footer/Footer";
+
 export default function Home() {
   return (
-    <>
-      <Header />
+    <main>
+      <Swiper />
 
-      <main>
-        <Swiper />
-        <BoxSlider
-          imgurl={"/applicationslider/applications.png"}
-          title={"عنوان محصول"}
-          header={"اپلیکیشن ها"}
-        />
-        <Banners />
-        <BoxSlider
-          imgurl={"/bookslider/ganj.jpg"}
-          title={"عنوان محصول"}
-          header={"کتاب ها"}
-        />
+      <BoxSlider
+        imgurl="/applicationslider/applications.png"
+        title="عنوان محصول"
+        header="اپلیکیشن ها"
+      />
 
-        <Categories />
-        <GraphicalSlider header={"فایل های گرافیکی"} />
+      <Banners />
 
-        <WebNews />
+      <BoxSlider
+        imgurl="/bookslider/ganj.jpg"
+        title="عنوان محصول"
+        header="کتاب ها"
+      />
 
-        <Footer/>
-      </main>
-    </>
+      <Categories />
+
+      <GraphicalSlider header="فایل های گرافیکی" />
+
+      <WebNews />
+    </main>
   );
 }
