8806e6af44f1749f2416b862284995e2aa720212
Samin <samin.mhr2004@gmail.com>
2026-09-04 18:59:39 +0330

    added discription.

--- components/productSidebar/SideBar.jsx
+++ components/productSidebar/SideBar.jsx
@@ -1,9 +1,11 @@
 import Btns from "./Btns";
+import Summery from "./Summery";
 
 const SideBar = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       <Btns />
+      <Summery />
       {/* <Input /> */}
       {/* <Summery />
       <MostViewd />
---
+++ components/productSidebar/Summery.jsx
@@ -0,0 +1,15 @@
+const Summery = () => {
+  return (
+    <div className="w-full flex flex-col justify-start bg-white! my-10 p-6 rounded-lg shadow-[0px_0px_5px_rgba(0,0,0,0.15)]">
+      <h3 className="font-bold py-2">معرفی کوتاه</h3>
+      <p className="line-clamp-4 text-justify text-zinc-500">
+        امروز یک توسعه‌دهنده تصمیم گرفت پروژه‌اش را کمی مرتب‌تر کند و چند خط کد
+        قدیمی را بازنویسی کند. بعد از چند ساعت کار، بالاخره همه‌چیز بدون خطا
+        اجرا شد و ظاهر سایت هم دقیقاً همان چیزی شد که انتظار داشت. حالا فقط
+        مانده بود یک فنجان قهوه و چند دقیقه استراحت قبل از شروع بخش بعدی پروژه.
+      </p>
+    </div>
+  );
+};
+
+export default Summery;
